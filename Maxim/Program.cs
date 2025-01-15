a9805019f63411cfecc7756006aef34c658f6bb8
Max Yeager <maxjeager@icloud.com>
2025-01-16 06:10:03 +0900

    Правка к задаче 2

--- Maxim/Program.cs
+++ Maxim/Program.cs
@@ -1,40 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-// TODO - убрать неиспользуемые библиотеки
+﻿
 namespace EducationVS
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //TODO - комментарии через 1 пробел оставляются
-            Console.Write("Введите Имя: "); // Отображение в консоли
-            string firstname = Console.ReadLine();                     // Ввод с консоли
-            Console.Write("Введите Фамилию: ");
-            string name = Console.ReadLine();
-            Console.Write("Введите возраст: ");
-            //TODO - нет проверки что введено. При вводе буквы будет ошибка
-            int age = int.Parse(Console.ReadLine());                  //Парсинг в целые числа
-            Console.Write("Введите свой вес: ");
-            double weight = double.Parse(Console.ReadLine());          //Парсинг в дробные числа
-            Console.Write("Работате? (Да/Нет) ");
-            string work = Console.ReadLine();
-            bool workA = (work == "да" || work == "Нет");
-            // TODO - Лишняя переменная. Её либо оформить константой, либо перенести значение
+            //TODO - комментарии через 1 пробел оставляются.
             const string Work0 = " Не определился ";
-            //TODO - Слишком длинная строка. Нужны переносы
-            Console.WriteLine($"\nВаше Имя: {firstname} \nВаша Фамилия: {name} \nВаш Возраст: {age} \nВаш Вес: {weight} \nРаботаете? {(workA ? work : Work0)}");   //Вывод на консоль
-            Console.ReadKey();
-            
-            //TODO - убрать лишние отступы
-
-
-
+            Console.Write("Введите Имя: "); // Отображение в консоли.
+            string? firstname = Console.ReadLine(); // Ввод с консоли.
+                Console.Write("Введите Фамилию: ");
+                string? name = Console.ReadLine();
+                    Console.Write("Введите возраст: ");           
+                    int age; // Парсинг в целые числа
+                    while (!int.TryParse(Console.ReadLine(), out age)) // ТруПарсинг возраста.
+                    Console.Write("Неверный ввод! \nВведите возраст: ");
+                    Console.WriteLine("Ваш возраст = {0}", age);
+                Console.Write("Введите свой вес: ");
+                double weight;
+                while (!double.TryParse(Console.ReadLine(), out weight)) // ТруПарсинг веса.
+                Console.Write("Неверный ввод! \nВведите свой вес: ");
+                Console.WriteLine("Ваш вес = {0}", weight);
+            Console.Write("Работате?: (Да/Нет) ");
+            string work = Console.ReadLine().ToUpper();
+            bool workA = (work == "ДА" || work == "НЕТ");    
+            Console.WriteLine($"\nВаше Имя: {firstname} \nВаша Фамилия: {name} \nВаш Возраст: {age}"); 
+            Console.WriteLine($"Ваш Вес: {weight} \nРаботаете? {(workA ? work : Work0)}"); // Вывод на консоль.
+            Console.ReadKey();            
         }
     }
 }
