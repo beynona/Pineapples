ac31c2178be1db790faa2e90ef8c19199d3697e0
taanderson451 <liansaigon@protonmail.com>
2025-02-11 00:12:08 +0300

    Консольное приложение для решения систем линейных уравнений 2-го и 3-го порядка.
    версия 0.3
    (правки + добавлена заготовка класса для работы с файловой системой)

--- Alexander/KramerMethodApp.cs
+++ Alexander/KramerMethodApp.cs
@@ -2,16 +2,20 @@ namespace Alexander;
 
 internal abstract class KramerMethodApp
 {
-    private const string StartMessage = "  Данная программа предназначена для расчета решения систем линейных уравнений \n" +
-                                        " методом Крамера. Принимает в качестве аргументов коэффициенты при неизвестных \n" +
-                                        " по порядку: слева-направо, сверху-вниз; и свободные коэффициенты. Реализованы \n" +
-                                        " расчеты решений для систем из 2-х и 3-х уравнений.                            \n" +
-                                        "                       _-=||| УПРАВЛЯЮЩИЕ КОМАНДЫ |||=-_                       \n" +
-                                        "+=============================================================================+\n" +
-                                        "|\"ВЫХОД\" - выход | \"ДОК\" - вывод описания | \"..........\" - последний результат|\n" +
-                                        "|=======| \"....\" - вывод всего лог-файла | \".....\" - очистка лог-файла |======|\n" +
-                                        "+=============================================================================+\n" +
-                                        "           ВВОД КОМАНД производить во время запроса порядка системы.           ";
+    private const string StartMessage = """
+                                         _____________________________________________________________________________
+                                        | Данная программа предназначена для расчета решения систем линейных уравнений|
+                                        |методом Крамера. Принимает в качестве аргументов коэффициенты при неизвестных|
+                                        |по порядку: слева-направо, сверху-вниз; и свободные коэффициенты. Реализованы|
+                                        |расчеты решений для систем из 2-х и 3-х уравнений.                           |
+                                        |                      _-=||| УПРАВЛЯЮЩИЕ КОМАНДЫ |||=-_                      |
+                                        +=============================================================================+
+                                        |"СТОП" - выход | "ДОК" - вывод описания | "..........." - последний результат|
+                                        |=======| "...." - вывод всего лог-файла | "....." - очистка лог-файла |======|
+                                        +=============================================================================+
+                                        |          ВВОД КОМАНД производить во время запроса порядка системы.          |
+                                        +-----------------------------------------------------------------------------+
+                                        """;
     private const string QuestionNumberEquations = "Введите количество уравнений в вашей системе: ";
     private const string InvalidInputMessage = "НЕВЕРНЫЙ ВВОД! Повторите ...";
     private const string EndMessage = "Bye...";
@@ -26,11 +30,13 @@ internal abstract class KramerMethodApp
             if (input is "2" or "3")
             {
                 int numberEquations = Convert.ToInt32(input);
-                double[][] matrix = DataGeneration(numberEquations);
-                double[][] result = Calculation(matrix, numberEquations);
-                PrintResult(result, matrix, numberEquations);
+                double[][] matrix = DataGeneration(ref numberEquations);
+                double[][] result = Calculation(ref matrix, ref numberEquations);
+                string message = PreparingOutput(ref matrix, ref result, ref numberEquations);
+                // WorkingWithLogs.WriteLog(ref message);
+                Console.WriteLine(message);
             }
-            else if (input is "выход")
+            else if (input is "стоп")
             {
                 Console.WriteLine("\n" + EndMessage);
                 break;
@@ -42,7 +48,7 @@ internal abstract class KramerMethodApp
         } while (true);
     }
 
-    private static double[][] DataGeneration(int numberEquations)
+    private static double[][] DataGeneration(ref readonly int numberEquations)
     {
         const string forInputMessageA = "\nВвод значений коэффициентов при неизвестных (слева-направо, сверху-вниз):";
         const string forInputMessageB = "\nВвод значений свободных коэффициентов:";
@@ -77,7 +83,7 @@ internal abstract class KramerMethodApp
         return matrix;
     }
 
-    private static double[][] Calculation(double[][] matrix, int numberEquations)
+    private static double[][] Calculation(ref readonly double[][] matrix, ref readonly int numberEquations)
     {
         double[] isDefined = [0, 1, 2];
         double[][] result = new double[3][];
@@ -116,47 +122,52 @@ internal abstract class KramerMethodApp
             bool equalityZero = true;
             for (int i = 1; i < result[1].Length; i++)
             {
-                if (result[1][i] != 0) equalityZero = false;
-                result[2][i - 1] = double.PositiveInfinity;
+                if (result[1][i] == 0) continue;
+                equalityZero = false;
+                break;
             }
             result[0][0] = equalityZero ? isDefined[1] : isDefined[2];
         }
         return result;
     }
-
-    private static void PrintResult(double[][] result, double[][] matrix, int numberEquations)
+    
+    private static string PreparingOutput(ref readonly double[][] matrix,
+            ref readonly double[][] result, ref readonly int numberEquations)
     {
-        const string systemDefinedMessage = " Система уравнений совместна и определена.\n Имеет единственное решение:";
-        const string systemNotDefinedMessage = " Система уравнений совместна, но не определена.\n Имеет бесчисленное множество решений.";
-        const string systemNotCompatibleMessage = " Система уравнений несовместна и не имеет решений.";
-        const string separator = "+=============================================================================+";
-        Console.WriteLine($"\n{separator}\n Введенная матрица имеет следующий вид:");
+        const string systemDefinedMessage = "Система уравнений совместна и определена.\nИмеет единственное решение:\n";
+        const string systemNotDefinedMessage = "Система уравнений совместна, но не определена.\n" +
+                                               "Имеет бесчисленное множество решений.\n";
+        const string systemNotCompatibleMessage = "Система уравнений несовместна и не имеет решений.\n";
+        const string separator = "==============================================================================+";
+        string matrixText = "\n", resultText = "\n";
         for (int i = 0; i < matrix[0].Length; i++)
         {
-            Console.Write(" {0}\t\t", Math.Round(matrix[0][i], 2, MidpointRounding.AwayFromZero));
+            matrixText += $"{Math.Round(matrix[0][i], 2, MidpointRounding.AwayFromZero)}\t\t";
             if ((i + 1) % numberEquations != 0) continue;
-            Console.Write("|\t");
-            if ((i + 1) / numberEquations == 1) Console.WriteLine(Math.Round(matrix[1][0], 2, MidpointRounding.AwayFromZero));
-            if ((i + 1) / numberEquations == 2) Console.WriteLine(Math.Round(matrix[1][1], 2, MidpointRounding.AwayFromZero));
-            if ((i + 1) / numberEquations == 3) Console.WriteLine(Math.Round(matrix[1][2], 2, MidpointRounding.AwayFromZero));
+            if ((i + 1) / numberEquations == 1)
+                matrixText += $"|\t{Math.Round(matrix[1][0], 2, MidpointRounding.AwayFromZero)}\n";
+            if ((i + 1) / numberEquations == 2)
+                matrixText += $"|\t{Math.Round(matrix[1][1], 2, MidpointRounding.AwayFromZero)}\n";
+            if ((i + 1) / numberEquations == 3)
+                matrixText += $"|\t{Math.Round(matrix[1][2], 2, MidpointRounding.AwayFromZero)}\n";
         }
-        Console.WriteLine(" Значения определителей следующие:");
         for (int i = 0; i < result[1].Length; i++)
-            Console.WriteLine($" D{i} = {Math.Round(result[1][i], 3, MidpointRounding.AwayFromZero)}");
+            resultText += $"D{i} = {Math.Round(result[1][i], 3, MidpointRounding.AwayFromZero)}\n";
         switch (result[0][0])
         {
             case 0:
-                Console.WriteLine(systemDefinedMessage);
+                resultText += systemDefinedMessage;
                 for (int i = 0; i < result[2].Length; i++)
-                    Console.WriteLine($" x{i+1} = {Math.Round(result[2][i], 3, MidpointRounding.AwayFromZero)}");
+                    resultText += $"x{i+1} = {Math.Round(result[2][i], 3, MidpointRounding.AwayFromZero)}\n";
                 break;
             case 1:
-                Console.WriteLine(systemNotDefinedMessage);
+                resultText += systemNotDefinedMessage;
                 break;
             case 2:
-                Console.WriteLine(systemNotCompatibleMessage);
+                resultText += systemNotCompatibleMessage;
                 break;
         }
-        Console.WriteLine(separator);
+        return $"\n{separator}\nВведенная матрица имеет следующий вид:{matrixText}" +
+               $"Значения определителей следующие:{resultText}{separator}";
     }
 }
---
+++ Alexander/WorkingWithLogs.cs
@@ -0,0 +1,24 @@
+namespace Alexander;
+
+internal abstract class WorkingWithLogs
+{
+    internal static void WriteLog(ref readonly string message)
+    {
+        
+    }
+
+    internal static void ReadLustLog()
+    {
+        
+    }
+
+    internal static void ReadLogs()
+    {
+        
+    }
+
+    internal static void ClearLogs()
+    {
+        
+    }
+}
