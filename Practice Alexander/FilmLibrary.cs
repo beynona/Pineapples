9b914c46bf9b74314bc4c59370356b2b431bf843
taanderson451 <liansaigon@protonmail.com>
2025-10-09 21:34:20 +0300

    application for developing theory by classes, structures and namespaces + possibly OOP

---
+++ Practice Alexander/FilmLibrary.cs
@@ -0,0 +1,24 @@
+namespace Alexander;
+
+internal abstract class FilmLibrary
+{
+    private const string StartMessage = """
+                                        +=============================================================================+
+                                        ||                            _____ ___  _    ___                            ||
+                                        ||                           |  ___|_ _/| |  |_ _/                           ||
+                                        ||                           | |_   | | | |   | |                            ||
+                                        ||                           |  _|  | | | |__ | |                            ||
+                                        ||                           |_|   |___\|____|___\                           ||
+                                        ||                                                                           ||
+                                        ||                   Вас приветствует FiLi (Film Library)!                   ||
+                                        ||       Приложение, своего рода библиотека, которую Вы создаете сами.       ||
+                                        ||                                                                           ||
+                                        +=============================================================================+
+                                        """;
+    internal static void StartApp()
+    {
+        Console.Clear();
+        Console.WriteLine(StartMessage);
+        Console.ReadKey();
+    }
+}
