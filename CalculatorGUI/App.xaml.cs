365d2a417728585d367903509ac372c2203916fd
taanderson451 <liansaigon@protonmail.com>
2025-12-06 20:49:25 +0300

    Added a working application with a graphical interface "Calculator GUI",
    written using Blazor Hybrid, which has a working version
    for WinPC and Android (with some nuances).

---
+++ CalculatorGUI/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace CalculatorGUI;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage())
+        {
+            Title = "Calculator",
+            Height = 700,
+            Width = 425,
+            MinimumHeight = 700,
+            MinimumWidth = 425
+        };
+    }
+}
---
+++ CalculatorGUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CalculatorGUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
